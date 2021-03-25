2c0fbab0e5fd071c5689b1efcb6a129b97aca90c
Admin <Admin@SHASHANKS-DELL>
2021-03-25 13:21:46 +0530

    trialclass

---
+++ src/trialrepository/Trialprojectpage1.java
@@ -0,0 +1,19 @@
+package trialrepository;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Trialprojectpage1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","E:\\Selenium material\\broswers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.practiceselenium.com/practice-form.html");
+	      driver.findElement(By.xpath("//*[@text()='Switch Commands']"));
+	      System.out.println(driver.findElement(By.xpath("//*[@text()='Wait Commands']")).getText());
+		
+	}
+
+}
